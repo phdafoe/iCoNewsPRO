169dc49b088fb168f59c82cd980413587d7b561c
Phdafoe <phdafoe@gmail.com>
2022-11-02 15:17:46 +0100

    Initial Commit

---
+++ iCoNews/iCoNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoNewsApp.swift
+//  iCoNews
+//
+//  Created by TECDATA ENGINEERING on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iCoNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
